71bfcf20c6c8631007bdeda3047b2124151ab0ac
Syndric <65517819+Syndric@users.noreply.github.com>
2025-08-07 14:13:24 -0400

    add: empty base files

---
+++ include/vl53l1x/vl53l1x_node.hpp
@@ -0,0 +1,10 @@
+#ifndef VL53L1X_VL53L1X_NODE_HPP_
+#define VL53L1X_VL53L1X_NODE_HPP_
+
+#include "vl53l1x/vl53l1x.hpp"
+#include <rclcpp/rclcpp.hpp>
+
+namespace vl53l1x {
+    class Vl53l1x : public rclcpp::No
+};
+#endif
