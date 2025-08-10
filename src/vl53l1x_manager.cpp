3be71cb6c15fc38281664f1a56033b856d00a887
Syndric <65517819+Syndric@users.noreply.github.com>
2025-08-10 00:30:45 -0400

    fix: build

--- src/vl53l1x_manager.cpp
+++ src/vl53l1x_manager.cpp
@@ -1,10 +1,10 @@
-#include "tca9548a/GenericI2CManager.hpp"
+#include "tca9548a/generic_i2c_manager.hpp"
 #include "rclcpp/rclcpp.hpp"
 
 int main(int argc, char *argv[])
 {
     rclcpp::init(argc, argv);
-    auto node = std::make_shared<tca9548a::GenericI2CManager>();
+    auto node = std::make_shared<tca9548a::GenericI2CManager>(rclcpp::NodeOptions());
     rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
